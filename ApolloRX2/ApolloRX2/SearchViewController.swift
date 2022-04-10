35e8c4862b51b14c7df962709b8f8881a49c93a1
Jasmine1418 <jasmine.makombe@gmail.com>
2022-04-10 17:25:38 -0400

    search api logic

--- ApolloRX2/ApolloRX2/SearchViewController.swift
+++ ApolloRX2/ApolloRX2/SearchViewController.swift
@@ -2,77 +2,63 @@
 //  SearchViewController.swift
 //  ApolloRX2
 //
-//  Created by Leah Clements on 3/24/22.
+//  Created by Jasmine Makombe on 4/10/22.
 //
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var searchTableView: UITableView!
+    
+    var medSearchResults = [[String:Any]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        searchTableView.dataSource = self
+        searchTableView.delegate = self
         // Do any additional setup after loading the view.
-    }
+        let url = URL(string: "https://rxnav.nlm.nih.gov/REST/displaynames.json")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                    
+                 self.medSearchResults = dataDictionary["displayTermsList"] as! [[String:Any]]
+                 
+                 self.searchTableView.reloadData()
+                 
+                    print(dataDictionary)
 
+                    // TODO: Get the array of movies
+                    // TODO: Store the movies in a property to use elsewhere
+                    // TODO: Reload your table view data
 
-    import UIKit
-    class SearchViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
-            @IBOutlet weak var tableView: UITableView!
-            @IBOutlet weak var searchBar: UISearchBar!
-    //
-            let data = ["Hydrocodone,acetaminophen", "Simvastatin,Zocor,", "Lisinopril, Qbrelis", "Levothyroxine, Synthroid",
-                "Amlodipine Besylate, Norvasc", "Omeprazole, Prilosec OTC", "Azithromycin, AzaSite", "Metformin , Glumetza",
-                "Hydrochlorothiazide, Microzide", "Omeprazole", "Warfarin", "Atorvastatin, Lipitor",
-                "Albuterol, Ventolin", "Losartan, Cozaar", "Amlodipine, Norvasc", "Amoxicillin, Amoxil",]
-
-            var filteredData: [String]!
-
-            override func viewDidLoad() {
-                super.viewDidLoad()
-                tableView.dataSource = self
-                searchBar.delegate = self
-                filteredData = data
-            }
-
-            func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-                let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath) as UITableViewCell
-                cell.textLabel?.text = filteredData[indexPath.row]
-                return cell
-            }
-
-            func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-                return filteredData.count
-            }
-
-            // This method updates filteredData based on the text in the Search Box
-            func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-                // When there is no text, filteredData is the same as the original data
-                // When user has entered text into the search box
-                // Use the filter method to iterate over all items in the data array
-                // For each item, return true if the item should be included and false if the
-                // item should NOT be included
-                filteredData = searchText.isEmpty ? data : data.filter { (item: String) -> Bool in
-                    // If dataItem matches the searchText, return true to include it
-                    return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
-                }
-                
-                tableView.reloadData()
-            }
+             }
         }
-
-        /*
-        // MARK: - Navigation
-
-        // In a storyboard-based application, you will often want to do a little preparation before navigation
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            // Get the new view controller using segue.destination.
-            // Pass the selected object to the new view controller.
-        }
-        */
-
+        task.resume()
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return medSearchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let drug = medSearchResults[indexPath.row]
+        let drugName = drug["term"] as! String
+        cell.textLabel!.text = "row:\(indexPath.row)"
+        
+        return cell
+    }
+    
+    
 
     /*
     // MARK: - Navigation

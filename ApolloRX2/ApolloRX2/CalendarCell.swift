3d1711bd282e3e5c68f082712529e6317db448b3
Nubia Johnson <nubia.johnson@bison.howard.edu>
2022-04-11 13:28:58 -0400

    Calendar

---
+++ ApolloRX2/ApolloRX2/CalendarCell.swift
@@ -0,0 +1,14 @@
+//
+//  CalenderCell.swift
+//  ApolloRX2
+//
+//  Created by Nubia Johnson on 4/10/22.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell
+{
+    @IBOutlet weak var dayOfMonth: UILabel!
+    
+}

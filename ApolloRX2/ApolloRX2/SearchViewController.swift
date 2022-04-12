a3390dbe0b65ba64aaf736079623b5c6b9542648
Jasmine1418 <jasmine.makombe@gmail.com>
2022-04-11 23:02:33 -0400

    api logic for search screen

--- ApolloRX2/ApolloRX2/SearchViewController.swift
+++ ApolloRX2/ApolloRX2/SearchViewController.swift
@@ -7,19 +7,17 @@
 
 import UIKit
 
-class SearchViewController: UIViewController{
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
     @IBOutlet weak var searchTableView: UITableView!
-    
-    var medSearchResultsArrayDict = [[String:Any]]()
     
     var medSearchResults = [[String:Any]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //searchTableView.dataSource = self
-        //searchTableView.delegate = self
+        searchTableView.dataSource = self
+        searchTableView.delegate = self
         // Do any additional setup after loading the view.
         let url = URL(string: "https://rxnav.nlm.nih.gov/REST/drugs.json?name=Lipitor")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -33,18 +31,15 @@ class SearchViewController: UIViewController{
                  
                  let drugGroup = dataDictionary["drugGroup"] as! [String:Any];
                  let conceptGroup = drugGroup["conceptGroup"] as! [[String:Any]];
-                 let conceptProperties = conceptGroup[1]
+                 let conceptGroupSecondElem = conceptGroup[1];
+                 let conceptProperties = conceptGroupSecondElem["conceptProperties"] as! [[String:Any]]
                  
-                 //let conceptGroup = drugGroup["conceptGroup"] as! [String:Any];
-                    
-                 //self.medSearchResultsArrayDict = dataDictionary["conceptGroup"] as! [[String:Any]]
-                 //var arrayConceptPropertiesArrayDict = self.medSearchResultsArrayDict[1]
+                 self.medSearchResults = conceptProperties
                  
-                 //self.medSearchResults = arrayConceptPropertiesArrayDict
                  
-                 //self.searchTableView.reloadData()
+                 self.searchTableView.reloadData()
                  
-                 print(conceptProperties)
+                 print(conceptGroup)
 
                     // TODO: Get the array of movies
                     // TODO: Store the movies in a property to use elsewhere
@@ -55,21 +50,22 @@ class SearchViewController: UIViewController{
         task.resume()
     }
     
-    /*
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return medSearchResults.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         
         let drug = medSearchResults[indexPath.row]
-        let drugName = drug["conceptProperties"] as! String
-        cell.textLabel!.text = "row:\(indexPath.row)"
+        let drugName = drug["name"] as! String
+        
+        cell.textLabel!.text = drugName
         
         return cell
     }
-    */
+    
     
 
     /*
